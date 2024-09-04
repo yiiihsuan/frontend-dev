ec963bb25fb043cdf22196fbe7aeea3f9a90285b
yihsuan <yiiihsuann@gmail.com>
2024-09-05 00:35:17 +0200

    fix: logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,79 +1,79 @@
-// import './App.css';
-
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import LoginPage from './pages/LoginPage';
-// import HomePage from './pages/HomePage';
-// import ProjectPage from './pages/ProjectPage';
-
-
-// function App() {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   const handleLogin = () => {
-//     setIsLoggedIn(true);
-//   };
-
-//   return (
-//     <Router>
-//       <div>
-//         <Routes>
-//             <Route path="/" element={isLoggedIn ? <Navigate to="/home" /> : <LoginPage onLogin={handleLogin} />} />
-//             <Route path="/home" element={isLoggedIn ? <HomePage /> : <Navigate to="/" />} />
-//             <Route path="/project/:projectId" element={isLoggedIn ? <ProjectPage /> : <Navigate to="/" />} />
-            
-            
-//             {/* <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
-//             <Route path="/home" element={<HomePage />} />
-//             <Route path="/project/:projectId" element={<ProjectPage />} /> */}
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-
-
-// export default App;
-
 import './App.css';
+
 import React, { useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import ProjectPage from './pages/ProjectPage';
-import LoginPage from './pages/LoginPage';
+
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // 管理登入狀態
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // 處理登入
-  const handleLogin = (isAuthenticated) => {
-    setIsLoggedIn(isAuthenticated);
+  const handleLogin = () => {
+    setIsLoggedIn(true);
   };
 
   return (
-    <div className="App">
-      <Routes>
-        {/* 登入狀態時，重定向到 /home；否則顯示 LoginPage */}
-        <Route 
-          path="/" 
-          element={isLoggedIn ? <Navigate to="/home" /> : <LoginPage onLogin={handleLogin} />} 
-        />
-        
-        {/* 只有登入狀態才能訪問 HomePage，否則重定向到登錄頁 */}
-        <Route 
-          path="/home" 
-          element={isLoggedIn ? <HomePage setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/" />} 
-        />
-        
-        {/* 只有登入狀態才能訪問 ProjectPage，否則重定向到登錄頁 */}
-        <Route 
-          path="/project/:projectId" 
-          element={isLoggedIn ? <ProjectPage setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/" />} 
-        />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+            <Route path="/" element={isLoggedIn ? <Navigate to="/home" /> : <LoginPage onLogin={handleLogin} />} />
+            <Route path="/home" element={isLoggedIn ? <HomePage setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/" />} />
+            <Route path="/project/:projectId" element={isLoggedIn ? <ProjectPage setIsLoggedIn={setIsLoggedIn}/> : <Navigate to="/" />} />
+            
+            
+            {/* <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/project/:projectId" element={<ProjectPage />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
+
+
 export default App;
+
+// import './App.css';
+// import React, { useState } from 'react';
+// import { Routes, Route, Navigate } from 'react-router-dom';
+// import HomePage from './pages/HomePage';
+// import ProjectPage from './pages/ProjectPage';
+// import LoginPage from './pages/LoginPage';
+
+// function App() {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false); // 管理登入狀態
+
+//   // 處理登入
+//   const handleLogin = (isAuthenticated) => {
+//     setIsLoggedIn(isAuthenticated);
+//   };
+
+//   return (
+//     <div className="App">
+//       <Routes>
+//         {/* 登入狀態時，重定向到 /home；否則顯示 LoginPage */}
+//         <Route 
+//           path="/" 
+//           element={isLoggedIn ? <Navigate to="/home" /> : <LoginPage onLogin={handleLogin} />} 
+//         />
+        
+//         {/* 只有登入狀態才能訪問 HomePage，否則重定向到登錄頁 */}
+//         <Route 
+//           path="/home" 
+//           element={isLoggedIn ? <HomePage setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/" />} 
+//         />
+        
+//         {/* 只有登入狀態才能訪問 ProjectPage，否則重定向到登錄頁 */}
+//         <Route 
+//           path="/project/:projectId" 
+//           element={isLoggedIn ? <ProjectPage setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/" />} 
+//         />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
