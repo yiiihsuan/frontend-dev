fc0de58dde5d30713b34f3df4d5a95e55cba0a0e
yihsuan <yiiihsuann@gmail.com>
2024-09-04 13:38:41 +0200

    fix logout

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -1,79 +1,6 @@
-// // SideBar.js
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
-// import { FaHome, FaUser, FaCog, FaSignOutAlt } from 'react-icons/fa';
-
-// const SidebarContainer = styled.div`
-//   width: ${(props) => (props.isOpen ? '200px' : '80px')}; /* Control the sidebar width */
-//   background-color: #333;
-//   padding: 20px;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   color: white;
-//   transition: width 0.3s;
-//   position: relative;
-
-//   &:hover {
-//     width: 200px;
-//   }
-// `;
-
-// const SidebarItem = styled(Link)`
-//   color: white;
-//   text-decoration: none;
-//   margin-bottom: 20px;
-//   font-family: 'Luckiest Guy', "Chocolate Classical Sans", sans-serif, cursive;
-//   font-size: 1.5em;
-//   display: flex;
-//   align-items: center;
-//   justify-content: ${(props) => (props.isOpen ? 'flex-start' : 'center')};
-//   width: 100%;
-
-//   & > span {
-//     margin-left: 10px;
-//     display: ${(props) => (props.isOpen ? 'inline' : 'none')};
-//   }
-
-//   &:hover {
-//     text-decoration: underline;
-//   }
-// `;
-
-// const Sidebar = ({ isOpen, setIsOpen }) => {
-
-  
-//   return (
-//     <SidebarContainer
-//       onMouseEnter={() => setIsOpen(true)}
-//       onMouseLeave={() => setIsOpen(false)}
-//       isOpen={isOpen}
-//     >
-//       <SidebarItem to="/" isOpen={isOpen}>
-//         <FaHome />
-//         <span>Home</span>
-//       </SidebarItem>
-//       <SidebarItem to="/profile" isOpen={isOpen}>
-//         <FaUser />
-//         <span>Profile</span>
-//       </SidebarItem>
-//       <SidebarItem to="/settings" isOpen={isOpen}>
-//         <FaCog />
-//         <span>Settings</span>
-//       </SidebarItem>
-//       <SidebarItem to="/logout" isOpen={isOpen}>
-//         <FaSignOutAlt />
-//         <span>Logout</span>
-//       </SidebarItem>
-//     </SidebarContainer>
-//   );
-// };
-
-// export default Sidebar;
-
+// SideBar.js
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom'; // 使用 useNavigate
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { FaHome, FaUser, FaCog, FaSignOutAlt } from 'react-icons/fa';
 
@@ -115,16 +42,8 @@ const SidebarItem = styled(Link)`
 `;
 
 const Sidebar = ({ isOpen, setIsOpen }) => {
-  const navigate = useNavigate(); // 使用 useNavigate 进行页面跳转
 
-  const handleLogout = () => {
-    // 清除 localStorage 的 token
-    localStorage.removeItem('token');
-
-    // 重定向到首页
-    navigate('/');
-  };
-
+  
   return (
     <SidebarContainer
       onMouseEnter={() => setIsOpen(true)}
@@ -143,8 +62,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
         <FaCog />
         <span>Settings</span>
       </SidebarItem>
-      {/* 修改的登出按钮 */}
-      <SidebarItem as="div" onClick={handleLogout} isOpen={isOpen}>
+      <SidebarItem to="/" isOpen={isOpen}>
         <FaSignOutAlt />
         <span>Logout</span>
       </SidebarItem>
@@ -153,3 +71,85 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
 };
 
 export default Sidebar;
+
+// import React from 'react';
+// import { Link, useNavigate } from 'react-router-dom'; // 使用 useNavigate
+// import styled from 'styled-components';
+// import { FaHome, FaUser, FaCog, FaSignOutAlt } from 'react-icons/fa';
+
+// const SidebarContainer = styled.div`
+//   width: ${(props) => (props.isOpen ? '200px' : '80px')}; /* Control the sidebar width */
+//   background-color: #333;
+//   padding: 20px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   color: white;
+//   transition: width 0.3s;
+//   position: relative;
+
+//   &:hover {
+//     width: 200px;
+//   }
+// `;
+
+// const SidebarItem = styled(Link)`
+//   color: white;
+//   text-decoration: none;
+//   margin-bottom: 20px;
+//   font-family: 'Luckiest Guy', "Chocolate Classical Sans", sans-serif, cursive;
+//   font-size: 1.5em;
+//   display: flex;
+//   align-items: center;
+//   justify-content: ${(props) => (props.isOpen ? 'flex-start' : 'center')};
+//   width: 100%;
+
+//   & > span {
+//     margin-left: 10px;
+//     display: ${(props) => (props.isOpen ? 'inline' : 'none')};
+//   }
+
+//   &:hover {
+//     text-decoration: underline;
+//   }
+// `;
+
+// const Sidebar = ({ isOpen, setIsOpen }) => {
+//   const navigate = useNavigate(); // 使用 useNavigate 进行页面跳转
+
+//   const handleLogout = () => {
+//     // 清除 localStorage 的 token
+//     localStorage.removeItem('token');
+
+//     // 重定向到首页
+//     navigate('/');
+//   };
+
+//   return (
+//     <SidebarContainer
+//       onMouseEnter={() => setIsOpen(true)}
+//       onMouseLeave={() => setIsOpen(false)}
+//       isOpen={isOpen}
+//     >
+//       <SidebarItem to="/" isOpen={isOpen}>
+//         <FaHome />
+//         <span>Home</span>
+//       </SidebarItem>
+//       <SidebarItem to="/profile" isOpen={isOpen}>
+//         <FaUser />
+//         <span>Profile</span>
+//       </SidebarItem>
+//       <SidebarItem to="/settings" isOpen={isOpen}>
+//         <FaCog />
+//         <span>Settings</span>
+//       </SidebarItem>
+//       {/* 修改的登出按钮 */}
+//       <SidebarItem as="div" onClick={handleLogout} isOpen={isOpen}>
+//         <FaSignOutAlt />
+//         <span>Logout</span>
+//       </SidebarItem>
+//     </SidebarContainer>
+//   );
+// };
+
+// export default Sidebar;
